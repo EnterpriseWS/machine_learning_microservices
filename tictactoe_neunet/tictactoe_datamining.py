e8c60115f8d3984e6ded36d4daa1143716aa48e5
EnterpriseWS <rcwang@enterprisews.net>
2019-07-04 18:11:01 -0700

    Cahnged dataset to classification style

--- tictactoe_neunet/tictactoe_datamining.py
+++ tictactoe_neunet/tictactoe_datamining.py
@@ -12,7 +12,8 @@ import json
 import csv
 
 input_template = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0.00]
-output_template = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+multioutput_template = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+singleoutput_template = [0]
 x_sequence = [1, 3, 5, 7, 9]
 o_sequence = [2, 4, 6, 8]
 Base = declarative_base()
@@ -20,7 +21,7 @@ engine = create_engine('sqlite:///tic-tac-toe.db', echo=False)
 Session = sessionmaker(bind=engine)
 
 
-def extract_sequence_focus_neuron(cvs_file, winner, begin_id, end_id):
+def extract_state_single_neuron(cvs_file, winner, begin_id, end_id):
     Base.metadata.create_all(engine)
     session = Session()
     counter = 0
@@ -46,14 +47,30 @@ def extract_sequence_focus_neuron(cvs_file, winner, begin_id, end_id):
                 if the_state.get(str(winner_sequence[index])) is None:
                     break
                 the_input = input_template.copy()
-                the_sequence = winner_sequence[index]
-                for sub_index in range(the_sequence):
+                the_output = singleoutput_template.copy()
+                curr_move = winner_sequence[index]
+                for sub_index in range(curr_move):
                     position_pair = the_state.get(str(sub_index+1))
                     if position_pair is None:
                         break
-                    the_input[sub_index] = int(position_pair[0])
-                the_input[len(input_template)-1] = round(the_sequence/step_count, 2)
-                print('Row ID ' + str(game_state.id) + ' convert to ' + str(the_input))
+                    if curr_move == sub_index + 1:
+                        # The most recent move in a state
+                        # This is the move to be predicted and cannot be
+                        # recorded as a previous move
+                        # Prediction a.k.a. feature state
+                        if position_pair[1] == 'X' or position_pair[1] == 'O':
+                            the_output[0] = int(position_pair[0])
+                    else:
+                        # All previous moves
+                        # Current state a.k.a. observal states
+                        if position_pair[1] == 'X':
+                            the_input[int(position_pair[0])-1] = 1
+                        elif position_pair[1] == 'O':
+                            the_input[int(position_pair[0])-1] = -1
+                print('Row ID ' + str(game_state.id)
+                      + ' convert to ' + str(the_input)
+                      + ' output as ' + str(the_output))
+                the_input.extend(the_output)
                 with open(cvs_file, 'a') as csv_fd:
                     writer = csv.writer(csv_fd)
                     writer.writerow(the_input)
@@ -66,7 +83,7 @@ def extract_sequence_focus_neuron(cvs_file, winner, begin_id, end_id):
     print(str(counter) + ' data set has been converted.')
 
 
-def extract_pattern_focus_neuron(cvs_file, winner, begin_id, end_id):
+def extract_state_multiple_neuron(cvs_file, winner, begin_id, end_id):
     Base.metadata.create_all(engine)
     session = Session()
     counter = 0
@@ -92,56 +109,7 @@ def extract_pattern_focus_neuron(cvs_file, winner, begin_id, end_id):
                 if the_state.get(str(winner_sequence[index])) is None:
                     break
                 the_input = input_template.copy()
-                the_sequence = winner_sequence[index]
-                for sub_index in range(the_sequence):
-                    position_pair = the_state.get(str(sub_index+1))
-                    if position_pair is None:
-                        break
-                    if position_pair[1] == 'X':
-                        the_input[int(position_pair[0])-1] = 1
-                    elif position_pair[1] == 'O':
-                        the_input[int(position_pair[0])-1] = 2
-                the_input[len(input_template)-1] = round(the_sequence/step_count, 2)
-                print('Row ID ' + str(game_state.id) + ' convert to ' + str(the_input))
-                with open(cvs_file, 'a') as csv_fd:
-                    writer = csv.writer(csv_fd)
-                    writer.writerow(the_input)
-            counter += 1
-
-    except:
-        logging.error('convert_to_neural_input query() error: ', sys.exc_info()[0])
-
-    session.close()
-    print(str(counter) + ' data set has been converted.')
-
-
-def extract_pattern_multineuron_output(cvs_file, winner, begin_id, end_id):
-    Base.metadata.create_all(engine)
-    session = Session()
-    counter = 0
-
-    try:
-        # Here game_state is equivalent to game_data variable in
-        # heuristic_microservice
-        game_states = session.query(GameState) \
-            .filter(GameState.winner == winner
-                    , GameState.id >= begin_id
-                    , GameState.id <= end_id) \
-            .order_by(GameState.id) \
-            .all()
-        for game_state in game_states:
-            the_state = json.loads(str(game_state.state))
-            step_count = len(the_state) - 1
-            winner_sequence = []
-            if winner == 'X':
-                winner_sequence = x_sequence.copy()
-            elif winner == 'O':
-                winner_sequence = o_sequence.copy()
-            for index in range(len(winner_sequence)):
-                if the_state.get(str(winner_sequence[index])) is None:
-                    break
-                the_input = input_template.copy()
-                the_output = output_template.copy()
+                the_output = multioutput_template.copy()
                 curr_move = winner_sequence[index]
                 for sub_index in range(curr_move):
                     position_pair = the_state.get(str(sub_index+1))
@@ -194,8 +162,8 @@ class GameState(Base):
 if __name__ == '__main__':
     import sys
     if len(sys.argv) >= 5:
-        extract_pattern_multineuron_output(sys.argv[1], sys.argv[2], int(sys.argv[3]), int(sys.argv[4]))
+        extract_state_single_neuron(sys.argv[1], sys.argv[2], int(sys.argv[3]), int(sys.argv[4]))
     elif len(sys.argv) == 4:
-        extract_pattern_multineuron_output(sys.argv[1], sys.argv[2], int(sys.argv[3]), 99999999)
+        extract_state_single_neuron(sys.argv[1], sys.argv[2], int(sys.argv[3]), 99999999)
     else:
         print('Error: Please enter at least two argument')
